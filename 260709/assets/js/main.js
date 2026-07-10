5a461c95c47b5b60acce78b9206a7d8f2a08b74b
Jurmiii <kje89105@gmail.com>
2026-07-10 18:02:24 +0900

    코딩연습
    웹사이트 - 반응형

--- 260709/assets/js/main.js
+++ 260709/assets/js/main.js
@@ -36,10 +36,35 @@ const scrollHeader = () =>{
 window.addEventListener('scroll', scrillHeader)
 
 /*=============== SHOW SCROLL UP ===============*/
-
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    // Add the .scroll-header class if the bottom scroll of the view
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+// Link the ID of each section (section id="home") to each link (a href="#home")
+// and activate the link with the class .active-link
+const scrollActive = () => {
+    // We get the position by scrolling down
+    const scrollY = window.scrollY
+
+    sections.forEach(section => {
+        const id = section.id,
+              top = section.offsetTop - 50,
+              height = section.offsetHeight,
+              link = document.querySelector('.nav__menu a[href*=' + id + ']')
+
+        if(!link) return
+
+        link.classList.toggle('active-link', scrollY > top && scrollY <= top + height)
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 
